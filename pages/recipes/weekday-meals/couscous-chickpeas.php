fc74ce426cd0b9058038646771656c04f26c58ff
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-11-11 22:06:22 +0000

    Add weekday meals, starting with couscous chickpeas

---
+++ pages/recipes/weekday-meals/couscous-chickpeas.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Couscous chickpeas</title>
+    <?php require "../../../includes/recipe-header.php"; ?>
+    <link rel="stylesheet" href="../../../main.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../../../index.html">
+            <img src="../../../favicon_io/android-chrome-512x512.png" width="30" height="30"
+                class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="../../../index.html">Home <span
+                        class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="weekday-meals.php">Weekday meals</a>
+
+            </div>
+        </div>
+    </nav>
+    
+    <div class="jumbotron text-center bg-light">
+        <h1>Couscous chickpeas</h1>
+        <p>Mmmmm...</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img class="img-fluid rounded" style="float:center" src="../../../images/couscous-chickpeas.png"
+                    alt="couscous">
+            </div>
+            <div class="col-sm-6">
+                <h3>Ingredients</h3>
+                <ul>
+                    <li>Couscous</li>
+                    <li>Broccoli</li>
+                    <li>Bacon/sausage</li>
+                    <li>Chopped tomatoes (baby ones are sweeter)</li>
+                    <li>Chicken</li>
+                    <li>Balsamic vinegar</li>
+                    <li>Onion</li>
+                    <li>1 tin chickpeas (or 125g dried)</li>
+                    <li>Mixed herbs</li>
+                </ul>
+                <ol>
+                <h3>Instructions</h3>
+                    <li>Ask the wife.</li>    
+                </ol>
+                <p>
+                    Serves 4.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer>
+    <?php require "../../../includes/recipe-footer.php"; ?>
+</footer>
+
+</html>
---
+++ pages/recipes/weekday-meals/weekday-meals.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Weekday meals</title>
+    <meta charset="utf-8">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="../../../favicon_io/site.webmanifest">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../../main.css">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../../../index.html">
+            <img src="../../../favicon_io/android-chrome-512x512.png" width="30" height="30"
+                class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="../../../index.html">Home <span
+                        class="sr-only">(current)</span></a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Categories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../pastas/pasta.php">Pasta</a>
+                        <a class="dropdown-item" href="../rice/rice.php">Rice + barley</a>
+                        <a class="dropdown-item" href="../soups/soup.php">Soup</a>
+                        <a class="dropdown-item" href="../salads/salad.php">Salads</a>
+                        <a class="dropdown-item" href="weekday-meals.php">Weekday meals</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="../smoothies/smoothies.php">Smoothies</a>
+                    </div>
+                </li>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron text-center bg-light">
+        <h1>Weekday meals</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <figure class="photo">
+                    <img src="../../../images/chicken-spring-onion.png" alt="sample image" />
+                    <figcaption>
+                        <h2>Chicken spring onion</h2>
+                    </figcaption>
+                    <a href="#"></a>
+                </figure>
+            </div>
+            <div class="col-sm">
+                <figure class="photo">
+                    <img src="../../../images/couscous-chickpeas.png" alt="sample image" />
+                    <figcaption>
+                        <h2>Couscous and chickpeas</h2>
+                    </figcaption>
+                    <a href="couscous-chickpeas.php"></a>
+                </figure>
+            </div>
+            <div class="col-sm">
+                <figure class="photo">
+                    <img src="../../../images/chicken-a-la-king.png" alt="sample image" />
+                    <figcaption>
+                        <h2>Chicken a la king</h2>
+                    </figcaption>
+                    <a href="#"></a>
+                </figure>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
