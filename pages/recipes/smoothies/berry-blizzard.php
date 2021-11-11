3334f2ef8ba46395a53d76d9b22df5aeb7af06ab
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-11-11 21:26:52 +0000

    Add smoothies and berry blizzard recipe

---
+++ pages/recipes/smoothies/berry-blizzard.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Berry blizzard</title>
+    <?php require "../../../includes/recipe-header.php"; ?>
+    <link rel="stylesheet" href="../../../main.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../../../index.html">
+            <img src="../../../favicon_io/android-chrome-512x512.png" width="30" height="30"
+                class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="../../../index.html">Home <span
+                        class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="smoothies.php">Smoothies</a>
+
+            </div>
+        </div>
+    </nav>
+    
+    <div class="jumbotron text-center bg-light">
+        <h1>Berry blizzard</h1>
+        <p>Mmmmm...</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <img class="img-fluid rounded" style="float:center" src="../../../images/strawberries.png"
+                    alt="berries">
+            </div>
+            <div class="col-sm-6">
+                <h3>Ingredients</h3>
+                <ul>
+                    <li>1 cup of mixed (frozen) berries</li>
+                    <li>1 banana (frozen is good, to keep things cold)</li>
+                    <li>4 Tbsp yoghurt</li>
+                    <li>Squirt of honey</li>
+                    <li>Dash of coconut</li>
+                    <li>½ tsp cinnamon</li>
+                    <li>Sprinkle of oats</li>
+                    <li>Milk to get it to 800ml</li>
+                    <li>Chia seeds on top</li>
+                </ul>
+                <ol>
+                    <li>Add the things to a blender, and whoosh. Feel free to add mint for a bit of summer freshness.</li>    
+                    <li>Pour and then sprinkle chia seeds on top (keeps them crunchy).</li>
+                </ol>
+                <p>
+                    Serves 2.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer>
+    <?php require "../../../includes/recipe-footer.php"; ?>
+</footer>
+
+</html>
